46dd1ed85f3f6d2d3258e59c1fbc450c796cd8ab
Richard Shea <rshea@thecubagroup.com>
2023-03-13 20:54:28 +1300

    Tweak to settings to try to make static work

--- dj_on_flyio/settings.py
+++ dj_on_flyio/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) 
+STATIC_ROOT = str(BASE_DIR.joinpath('static'))
+#STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
