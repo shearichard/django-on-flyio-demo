e4306d9b82b234bf3e171bd5cd03d27c71036ac2
Richard Shea <rshea@thecubagroup.com>
2023-03-14 22:28:23 +1300

    Tweak of settings.py to make static work as it should (at least locally)

--- dj_on_flyio/settings.py
+++ dj_on_flyio/settings.py
@@ -121,12 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR.joinpath('static'))
-#STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) 
+STATICFILES_DIRS = ( str(BASE_DIR.joinpath('static')), ) 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
